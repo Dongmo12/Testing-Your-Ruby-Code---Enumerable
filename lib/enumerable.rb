8c4b727cd8e36d3e0ed7d18debe4ac4129ceecaa
Ibrahim Dauda <ibrolive@gmail.com>
2019-10-04 16:53:55 +0100

    fixed more linter errors

--- lib/enumerable.rb
+++ lib/enumerable.rb
@@ -1,82 +1,75 @@
 module Enumerable
 
-    def my_each
-        for x in 0...self.length
-            yield(self[x])
-            x += 1
-        end
+  def my_each
+    for x in 0...self.length
+      yield(self[x])
+      x += 1
     end
+  end
 
-    def my_each_index
-        for x in 0...self.length
-            yield(self[x],x)
-            x += 1
-        end
+  def my_each_index
+    for x in 0...self.length
+      yield(self[x],x)
+      x += 1
     end
+  end
 
-    def my_select
-        select_arr = []
-        self.my_each{|x| select_arr << x if yield(x)}
-        select_arr
+  def my_select
+    select_arr = []
+    self.my_each{|x| select_arr << x if yield(x)}
+    select_arr
+  end
+
+  def my_all?
+    initial = false
+    self.my_each {|elem| yield(elem) ? initial = true : initial = false }
+    initial
+  end
+
+  def my_any?
+    self.my_each do |x|
+      return true if yield(x)
     end
+    false
+  end
 
-    def my_all?
-        initial = false
-        self.my_each {|elem| yield(elem) ? initial = true : initial = false }
-        initial
+  def my_none?
+    num = true
+    self.my_each{ |x| num = false if yield(x) }
+    num
+  end
+
+  def my_count num = nil
+    count = 0
+    if num 
+      self.my_each {|elem| count += 1 if elem == num}
+    elsif block_given?
+      self.my_each {|elem| count += 1 if yield(elem)}
+    else
+      count = self.length
     end
-    
-    def my_any?
-        self.my_each do |x|
-          return true if yield(x)
-        end
-        false
+    count
+  end
+
+  def my_map
+    array = []
+    self.my_each do |x|
+      yield(x) ? (array << true) : (array << false)
     end
-    
+    array
+  end
 
-    def my_none?
-        num = true
-        self.my_each{ |x| num = false if yield(x) }
-        num
+  def my_inject
+    start = nil
+    start == nil ? result = self[0] : result = start
+
+    for i in 1..self.length - 1
+      result = yield(result, self[i])
     end
+    result
+  end
 
-    def my_count num = nil
-        count = 0
-          if num 
-            self.my_each {|elem| count += 1 if elem == num}
-          elsif block_given?
-            self.my_each {|elem| count += 1 if yield(elem)}
-          else
-            count = self.length
-          end 
-        count
-    end
-
-    def my_map
-        array = []
-        self.my_each do |x|
-            yield(x) ? (array << true) : (array << false)
-        end
-        array
-    end
-
-    def my_inject 
-        start = nil
-        start == nil ? result = self[0] : result = start
-    
-        for i in 1..self.length - 1 
-          result = yield(result,self[i])
-        end 
-        result
-    end
-    
-
-        
-    def multiply_els(arr)
-
-        arr.my_inject { |memo, e| memo * e }
-  
-    end
-
+  def multiply_els(arr)
+    arr.my_inject { |memo, e| memo * e }
+  end
 end
-
